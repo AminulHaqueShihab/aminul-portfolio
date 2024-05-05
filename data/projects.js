f63b61cc753964f5040cc06dd3d6e4e8585f93ce
Md Aminul Haque <61130539+AminulHaqueShihab@users.noreply.github.com>
2024-05-05 11:48:45 +0600

    Projects added

--- data/projects.js
+++ data/projects.js
@@ -1,274 +1,313 @@
 const projects = [
-  {
-    name: "ULinks.io",
-    description:
-      "ULinks.io is a platform for students to browse & upload academic group chats to stay connected during online school",
-    pic: "/ulinks.PNG",
-    tech: ["React", "GraphQL", "Node.js"],
-    links: [
-      {
-        label: "Github",
-        icon: "github",
-        url: "https://github.com/jcserv/ulinks",
-      },
-    ],
-  },
-  {
-    name: "UTM TA Application System",
-    description:
-      "UTAP is the official platform for all MCS TA postings @ UTM enabling students and instructors to apply for postings and manage applications.",
-    pic: "/utap.png",
-    tech: ["React", "Node.js"],
-    links: [
-      {
-        label: "Website",
-        icon: "link",
-        url: "https://utap.utm.utoronto.ca/utap/",
-      },
-    ],
-  },
-  {
-    name: "PostureAI",
-    description:
-      "PostureAI is an AI powered posture improvement web app, used by 200 monthly active users, that helps you maintain healthy posture while working.",
-    subtitle: "1st Place in Best Use of GCP",
-    pic: "/postureai.png",
-    tech: ["React", "TypeScript"],
-    links: [
-      {
-        label: "Website",
-        icon: "link",
-        url: "https://postureai.tech/",
-      },
-      {
-        label: "Github",
-        icon: "github",
-        url: "https://github.com/jcserv/PostureAI",
-      },
-      {
-        label: "Demo",
-        icon: "youtube",
-        url: "https://www.youtube.com/watch?v=SqUmP6npKYM",
-      },
-      {
-        label: "Devpost",
-        icon: "devpost",
-        url: "https://devpost.com/software/placeholder-9gjdst",
-      },
-    ],
-  },
+	{
+		name: 'Bazar365',
+		description: `As part of a dynamic team, I contributed to a comprehensive e-commerce solution designed to elevate user engagement and streamline performance. Utilizing Next.js, TypeScript, Chakra UI, and Redux Toolkit, the application delivers a responsive interface, customizable dark mode, and fluid animations. My role involved frontend development tasks, such as integrating APIs, managing state, and crafting visually appealing UI components.`,
+		pic: '/bazar365.png',
+		tech: [
+			'Next.Js',
+			'TypeScript',
+			'Chakra Ui',
+			'Redux Toolkit',
+			'Framer Motion',
+		],
+		links: [
+			{
+				label: 'Website',
+				icon: 'link',
+				url: 'https://www.bazar365.com/',
+			},
+		],
+	},
+	{
+		name: 'Artisan - E-commerce Website Clone',
+		description: `This is an Artisan E-commerce website clone, designed with a focus on responsiveness and user experience. Built with Next.js, TypeScript, Chakra UI, Framer Motion, and Redux Toolkit, the frontend offers a seamless browsing experience with intuitive product filtering by categories and price. Created solely for educational purposes, the site utilizes Redux Toolkit for efficient state management, ensuring a smooth and interactive interface for users to explore and engage with products dynamically.`,
+		pic: '/artisan.png',
+		tech: [
+			'Next.Js',
+			'TypeScript',
+			'Chakra Ui',
+			'Redux Toolkit',
+			'Framer Motion',
+		],
+		links: [
+			{
+				label: 'Website',
+				icon: 'link',
+				url: 'https://artisan-clone.vercel.app/',
+			},
+			{
+				label: 'Github',
+				icon: 'github',
+				url: 'https://github.com/AminulHaqueShihab/artisan-clone',
+			},
+		],
+	},
+	{
+		name: 'YumCart - Food Ordering Website',
+		description: `This responsive food ordering website frontend, developed using React and Tailwind CSS, offers users an intuitive interface for browsing and filtering food items by categories and price. Leveraging React hooks like useState(), the site ensures smooth functionality and dynamic user interaction.`,
+		pic: '/yumcart.png',
+		tech: ['React.js', 'Tailwind CSS'],
+		links: [
+			{
+				label: 'Website',
+				icon: 'link',
+				url: 'https://yumcart-online.web.app/',
+			},
+			{
+				label: 'Github',
+				icon: 'github',
+				url: 'https://github.com/AminulHaqueShihab/yumcart-food-delivery-app-reactjs-tailwind',
+			},
+		],
+	},
+	{
+		name: 'Scary Pacman',
+		description: `This is a simple javascript based pacman game which is a lot different from the usual pacman game we have played in our childhood. It is developed using HTML, CSS and JavaScript.`,
+		pic: '/pacman.png',
+		tech: ['HTML', 'CSS', 'JavaScript'],
+		links: [
+			{
+				label: 'Website',
+				icon: 'link',
+				url: 'https://aminulhaqueshihab.github.io/Scary-Pacman-game/',
+			},
+			{
+				label: 'Github',
+				icon: 'github',
+				url: 'https://github.com/AminulHaqueShihab/Scary-Pacman-game',
+			},
+		],
+	},
+	{
+		name: 'Babbu Toy Shop - Simple e-commerce Website',
+		description: `This simple e-commerce website frontend, crafted with React and CSS, integrates essential concepts like routing through React Router Dom and state management via useContext(), enabling seamless global state control. Users enjoy a fluid shopping experience, with the ability to add multiple items to their cart, review and remove selections, and view the final price for their purchases, all within an intuitive interface.`,
+		pic: '/babbu.png',
+		tech: ['React.Js', 'CSS'],
+		links: [
+			{
+				label: 'Website',
+				icon: 'link',
+				url: 'https://babbu.netlify.app/',
+			},
+			{
+				label: 'Github',
+				icon: 'github',
+				url: 'https://github.com/AminulHaqueShihab/e-commerce_shopping_cart',
+			},
+		],
+	},
 ];
 
 export const extraProjects = [
-  {
-    name: "SQL Automarker",
-    type: "Research",
-    description:
-      "Microservices architecture enabling SQL automarking with admin panel",
-    tech: ["React", "Node.js", "MongoDB", "Docker"],
-    links: [
-      {
-        label: "Project",
-        icon: "link",
-        url: "https://www.michaelliut.ca/projects.html",
-      },
-    ],
-  },
-  {
-    name: "ResumeWords",
-    type: "Personal",
-    description:
-      "A tool that identifies keywords in job postings using ML that will improve your chances of getting a job.",
-    tech: ["React", "TypeScript", "Docker"],
-    links: [
-      {
-        label: "Website",
-        icon: "link",
-        url: "https://resume-words.com/",
-      },
-      {
-        label: "Github",
-        icon: "github",
-        url: "https://github.com/ShahmeerShahid/ResumeWords",
-      },
-    ],
-  },
-  {
-    name: "AnonBot",
-    type: "Personal",
-    description: "Discord bot for anonymous messaging, with 2100 messages sent",
-    tech: ["Node.js"],
-    links: [
-      {
-        label: "Github",
-        icon: "github",
-        url: "https://github.com/white-van/WVAnonBot",
-      },
-    ],
-  },
-  {
-    name: "Citrade",
-    type: "Work",
-    description:
-      "Citrade is a proof-of-concept for a retail investment brokerage, equipped with an easy-to-use UI and detailed analytics.",
-    tech: ["React", "AWS"],
-    links: [
-      {
-        label: "Github",
-        icon: "github",
-        url: "https://github.com/jcserv/citrade-frontend",
-      },
-      {
-        label: "Website",
-        icon: "link",
-        url: "https://main.d2mbmzkt6yj28g.amplifyapp.com/",
-      },
-    ],
-  },
-  {
-    name: "UTM CSSC Website",
-    type: "Work",
-    description:
-      "The CSSC Website is an open space for UTM CS students to ask questions, use academic tools, and get connected within the CS community.",
-    tech: ["Vue"],
-    links: [
-      {
-        label: "Github",
-        icon: "github",
-        url: "https://github.com/utm-cssc/website",
-      },
-      {
-        label: "Website",
-        icon: "link",
-        url: "https://cssc.utm.utoronto.ca/",
-      },
-    ],
-  },
-  {
-    name: "CollabCloud",
-    type: "School",
-    description:
-      "Social networking platform allowing users to find collaborators for software projects.",
-    tech: ["React", "Node.js", "PostgreSQL"],
-    links: [
-      {
-        label: "Github",
-        icon: "github",
-        url: "https://github.com/collabcloud/project-collabcloud",
-      },
-      {
-        label: "Demo",
-        icon: "youtube",
-        url: "https://youtu.be/bPAOhb8r5Co",
-      },
-    ],
-  },
-  {
-    name: "freeflo.io",
-    type: "Hackathon",
-    description:
-      "Full stack web application enabling collaborative & open source journalism",
-    tech: ["React", "Redux", "Next.js"],
-    links: [
-      {
-        label: "Github",
-        icon: "github",
-        url: "https://github.com/white-van/freeflo.io",
-      },
-      {
-        label: "Demo",
-        icon: "youtube",
-        url: "https://www.youtube.com/watch?v=pXDF2kgQGtk",
-      },
-    ],
-  },
-  {
-    name: "Portfolio V1",
-    type: "Personal",
-    description: "My old personal portfolio, deployed with Heroku",
-    tech: ["React", "Express", "MongoDB"],
-    links: [
-      {
-        label: "Github",
-        icon: "github",
-        url: "https://github.com/jcserv/portfoliov1",
-      },
-      {
-        label: "Website",
-        icon: "link",
-        url: "https://infinite-inlet-11529.herokuapp.com/",
-      },
-    ],
-  },
-  {
-    name: "HarMoney",
-    type: "Hackathon",
-    description: "Mobile application enabling users to split transactions",
-    tech: ["React Native", "Node.js"],
-    links: [
-      {
-        label: "Github",
-        icon: "github",
-        url: "https://github.com/matthuynh/harmoney",
-      },
-      {
-        label: "Demo",
-        icon: "youtube",
-        url: "https://youtu.be/s13y0c__6u4",
-      },
-      {
-        label: "Devpost",
-        icon: "devpost",
-        url: "https://devpost.com/software/harmoney-ci42yp",
-      },
-    ],
-  },
-  {
-    name: "InsurApp",
-    type: "Hackathon",
-    description: "Micro insurance mobile app using image recognition",
-    tech: ["Android Studio", "Flask", "AWS"],
-    links: [
-      {
-        label: "Github",
-        icon: "github",
-        url: "https://github.com/jcserv/InsurApp",
-      },
-    ],
-  },
-  {
-    name: "VapeSafe",
-    type: "Hackathon",
-    description:
-      "Mobile + hardware solution allowing users to quit vaping, won 3rd Place",
-    tech: ["Android Studio", "Arduino"],
-    links: [
-      {
-        label: "Github",
-        icon: "github",
-        url: "https://github.com/leviaviv28/VapeSafe-EngHack2019",
-      },
-      {
-        label: "Devpost",
-        icon: "devpost",
-        url: "https://devpost.com/software/vapesafe/",
-      },
-    ],
-  },
-  {
-    name: "ETH-Aion Atomic Swap",
-    type: "Hackathon",
-    description:
-      "Atomic swap protocol for ETH & Aion cryptocurrencies, earning Top 6",
-    tech: ["Java"],
-    links: [
-      {
-        label: "Github",
-        icon: "github",
-        url: "https://github.com/jcserv/AtomicSwap",
-      },
-    ],
-  },
+	{
+		name: 'SQL Automarker',
+		type: 'Research',
+		description:
+			'Microservices architecture enabling SQL automarking with admin panel',
+		tech: ['React', 'Node.js', 'MongoDB', 'Docker'],
+		links: [
+			{
+				label: 'Project',
+				icon: 'link',
+				url: 'https://www.michaelliut.ca/projects.html',
+			},
+		],
+	},
+	{
+		name: 'ResumeWords',
+		type: 'Personal',
+		description:
+			'A tool that identifies keywords in job postings using ML that will improve your chances of getting a job.',
+		tech: ['React', 'TypeScript', 'Docker'],
+		links: [
+			{
+				label: 'Website',
+				icon: 'link',
+				url: 'https://resume-words.com/',
+			},
+			{
+				label: 'Github',
+				icon: 'github',
+				url: 'https://github.com/ShahmeerShahid/ResumeWords',
+			},
+		],
+	},
+	{
+		name: 'AnonBot',
+		type: 'Personal',
+		description: 'Discord bot for anonymous messaging, with 2100 messages sent',
+		tech: ['Node.js'],
+		links: [
+			{
+				label: 'Github',
+				icon: 'github',
+				url: 'https://github.com/white-van/WVAnonBot',
+			},
+		],
+	},
+	{
+		name: 'Citrade',
+		type: 'Work',
+		description:
+			'Citrade is a proof-of-concept for a retail investment brokerage, equipped with an easy-to-use UI and detailed analytics.',
+		tech: ['React', 'AWS'],
+		links: [
+			{
+				label: 'Github',
+				icon: 'github',
+				url: 'https://github.com/jcserv/citrade-frontend',
+			},
+			{
+				label: 'Website',
+				icon: 'link',
+				url: 'https://main.d2mbmzkt6yj28g.amplifyapp.com/',
+			},
+		],
+	},
+	{
+		name: 'UTM CSSC Website',
+		type: 'Work',
+		description:
+			'The CSSC Website is an open space for UTM CS students to ask questions, use academic tools, and get connected within the CS community.',
+		tech: ['Vue'],
+		links: [
+			{
+				label: 'Github',
+				icon: 'github',
+				url: 'https://github.com/utm-cssc/website',
+			},
+			{
+				label: 'Website',
+				icon: 'link',
+				url: 'https://cssc.utm.utoronto.ca/',
+			},
+		],
+	},
+	{
+		name: 'CollabCloud',
+		type: 'School',
+		description:
+			'Social networking platform allowing users to find collaborators for software projects.',
+		tech: ['React', 'Node.js', 'PostgreSQL'],
+		links: [
+			{
+				label: 'Github',
+				icon: 'github',
+				url: 'https://github.com/collabcloud/project-collabcloud',
+			},
+			{
+				label: 'Demo',
+				icon: 'youtube',
+				url: 'https://youtu.be/bPAOhb8r5Co',
+			},
+		],
+	},
+	{
+		name: 'freeflo.io',
+		type: 'Hackathon',
+		description:
+			'Full stack web application enabling collaborative & open source journalism',
+		tech: ['React', 'Redux', 'Next.js'],
+		links: [
+			{
+				label: 'Github',
+				icon: 'github',
+				url: 'https://github.com/white-van/freeflo.io',
+			},
+			{
+				label: 'Demo',
+				icon: 'youtube',
+				url: 'https://www.youtube.com/watch?v=pXDF2kgQGtk',
+			},
+		],
+	},
+	{
+		name: 'Portfolio V1',
+		type: 'Personal',
+		description: 'My old personal portfolio, deployed with Heroku',
+		tech: ['React', 'Express', 'MongoDB'],
+		links: [
+			{
+				label: 'Github',
+				icon: 'github',
+				url: 'https://github.com/jcserv/portfoliov1',
+			},
+			{
+				label: 'Website',
+				icon: 'link',
+				url: 'https://infinite-inlet-11529.herokuapp.com/',
+			},
+		],
+	},
+	{
+		name: 'HarMoney',
+		type: 'Hackathon',
+		description: 'Mobile application enabling users to split transactions',
+		tech: ['React Native', 'Node.js'],
+		links: [
+			{
+				label: 'Github',
+				icon: 'github',
+				url: 'https://github.com/matthuynh/harmoney',
+			},
+			{
+				label: 'Demo',
+				icon: 'youtube',
+				url: 'https://youtu.be/s13y0c__6u4',
+			},
+			{
+				label: 'Devpost',
+				icon: 'devpost',
+				url: 'https://devpost.com/software/harmoney-ci42yp',
+			},
+		],
+	},
+	{
+		name: 'InsurApp',
+		type: 'Hackathon',
+		description: 'Micro insurance mobile app using image recognition',
+		tech: ['Android Studio', 'Flask', 'AWS'],
+		links: [
+			{
+				label: 'Github',
+				icon: 'github',
+				url: 'https://github.com/jcserv/InsurApp',
+			},
+		],
+	},
+	{
+		name: 'VapeSafe',
+		type: 'Hackathon',
+		description:
+			'Mobile + hardware solution allowing users to quit vaping, won 3rd Place',
+		tech: ['Android Studio', 'Arduino'],
+		links: [
+			{
+				label: 'Github',
+				icon: 'github',
+				url: 'https://github.com/leviaviv28/VapeSafe-EngHack2019',
+			},
+			{
+				label: 'Devpost',
+				icon: 'devpost',
+				url: 'https://devpost.com/software/vapesafe/',
+			},
+		],
+	},
+	{
+		name: 'ETH-Aion Atomic Swap',
+		type: 'Hackathon',
+		description:
+			'Atomic swap protocol for ETH & Aion cryptocurrencies, earning Top 6',
+		tech: ['Java'],
+		links: [
+			{
+				label: 'Github',
+				icon: 'github',
+				url: 'https://github.com/jcserv/AtomicSwap',
+			},
+		],
+	},
 ];
 
 export default projects;
