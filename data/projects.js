899b22560f7fdec39ceef9ed8c7e22f3ca5b1356
Md Aminul Haque <61130539+AminulHaqueShihab@users.noreply.github.com>
2024-11-25 17:45:59 +0600

    Job updated

--- data/projects.js
+++ data/projects.js
@@ -27,6 +27,52 @@ const projects = [
 		],
 	},
 	{
+		name: 'Fish & Meat Club',
+		description: `E-commerce solution with Responsive UI, Framer Motion animations and built an Admin Dashboard for
+ streamlined product and order management. Implemented JWT authentication for secure user login and Redux Toolkit for state management. 
+ Created RESTful APIs and managed MongoDB for backend operations and scalable data storage. 
+ Deployed for production and maintained the website for optimal performance.
+`,
+		pic: '/fishnmeat.png',
+		tech: [
+			'Next.Js',
+			'Express.Js',
+			'MongoDB',
+			'Chakra Ui',
+			'Redux Toolkit',
+			
+		],
+		links: [
+			{
+				label: 'Website',
+				icon: 'link',
+				url: 'https://www.fishnmeatclub.com/',
+			},
+		],
+	},
+	{
+		name: 'Shohoz Air',
+		description: `Contributed to integrating external flight API services. Worked on the flight booking, reissuing, refunding, and payment systems. 
+		Developed advanced flight data filtering, a dynamic Markup system, and a complete payment pipeline. 
+		Built the backend and admin CMS for the platform.
+`,
+		pic: '/shohoz.png',
+		tech: [
+			'React.Js',
+			'Express.Js',
+			'Sequelize',
+			'Joi',
+			'Redux Toolkit',
+		],
+		links: [
+			{
+				label: 'Website',
+				icon: 'link',
+				url: 'https://air.shohoz.com/',
+			},
+		],
+	},
+	{
 		name: 'Bazar365',
 		description: `As part of a dynamic team, I contributed to a comprehensive e-commerce solution designed to elevate user engagement and streamline performance. Utilizing Next.js, TypeScript, Chakra UI, and Redux Toolkit, the application delivers a responsive interface, customizable dark mode, and fluid animations. My role involved frontend development tasks, such as integrating APIs, managing state, and crafting visually appealing UI components.`,
 		pic: '/bazar365.png',
@@ -45,6 +91,28 @@ const projects = [
 			},
 		],
 	},
+	{
+		name: 'Scary Pacman',
+		description: `This is a simple javascript based pacman game which is a lot different from the usual pacman game we have played in our childhood. It is developed using HTML, CSS and JavaScript.`,
+		pic: '/pacman.png',
+		tech: ['HTML', 'CSS', 'JavaScript'],
+		links: [
+			{
+				label: 'Website',
+				icon: 'link',
+				url: 'https://aminulhaqueshihab.github.io/Scary-Pacman-game/',
+			},
+			{
+				label: 'Github',
+				icon: 'github',
+				url: 'https://github.com/AminulHaqueShihab/Scary-Pacman-game',
+			},
+		],
+	},
+	
+];
+
+export const extraProjects = [
 	{
 		name: 'Artisan - E-commerce Website Clone',
 		description: `This is an Artisan E-commerce website clone, designed with a focus on responsiveness and user experience. Built with Next.js, TypeScript, Chakra UI, Framer Motion, and Redux Toolkit, the frontend offers a seamless browsing experience with intuitive product filtering by categories and price. Created solely for educational purposes, the site utilizes Redux Toolkit for efficient state management, ensuring a smooth and interactive interface for users to explore and engage with products dynamically.`,
@@ -88,24 +156,6 @@ const projects = [
 		],
 	},
 	{
-		name: 'Scary Pacman',
-		description: `This is a simple javascript based pacman game which is a lot different from the usual pacman game we have played in our childhood. It is developed using HTML, CSS and JavaScript.`,
-		pic: '/pacman.png',
-		tech: ['HTML', 'CSS', 'JavaScript'],
-		links: [
-			{
-				label: 'Website',
-				icon: 'link',
-				url: 'https://aminulhaqueshihab.github.io/Scary-Pacman-game/',
-			},
-			{
-				label: 'Github',
-				icon: 'github',
-				url: 'https://github.com/AminulHaqueShihab/Scary-Pacman-game',
-			},
-		],
-	},
-	{
 		name: 'Babbu Toy Shop - Simple e-commerce Website',
 		description: `This simple e-commerce website frontend, crafted with React and CSS, integrates essential concepts like routing through React Router Dom and state management via useContext(), enabling seamless global state control. Users enjoy a fluid shopping experience, with the ability to add multiple items to their cart, review and remove selections, and view the final price for their purchases, all within an intuitive interface.`,
 		pic: '/babbu.png',
@@ -123,9 +173,6 @@ const projects = [
 			},
 		],
 	},
-];
-
-export const extraProjects = [
 	{
 		name: 'Photography Portfolio Website',
 		type: 'Academy Project',
